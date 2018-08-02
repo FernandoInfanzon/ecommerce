61a4a093c9cd0c5a87c6bbd1c15221eb5c63642c
Fernando Infanzon <luisfernandoim@gmail.com>
2018-08-02 17:29:28 -0500

    se agregó estilos al header

--- vistas/modulos/header.php
+++ vistas/modulos/header.php
@@ -1,6 +1,8 @@
 <!--=========================
 =            TOP            =
 ==========================-->
+<div class="container-fluid barraSuperior" id="top">
+
 <div class="container">
 	<div class="row">
 		
@@ -46,28 +48,28 @@
 		</div>	
 		<!--====  End of Section SOCIAL  ====-->
 		
-		<!--==============================
-		=            REGISTRO            =
-		===============================-->
-		<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 registro">
-			<ul>
-				<li>
-					<a href="#modalIngreso" data-toggle="modal">Log in</a>
-				</li>
-				<li>|</li>
-				<li>
-					<a href="#modalRegistro" data-toggle="modal">Sign In</a>
-				</li>
+			<!--==============================
+			=            REGISTRO            =
+			===============================-->
+			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 registro">
+				<ul>
+					<li>
+						<a href="#modalIngreso" data-toggle="modal">Log in</a>
+					</li>
+					<li>|</li>
+					<li>
+						<a href="#modalRegistro" data-toggle="modal">Sign In</a>
+					</li>
 
-			</ul>
-		</div>
-		<!--====  End of REGISTRO  ====-->
-			
+				</ul>
+			</div>
+			<!--====  End of REGISTRO  ====-->
+				
 
 		
+		</div>
 	</div>
 </div>
-
 
 <!--====  End of TOP  ====-->
 
@@ -75,13 +77,14 @@
 =            HEADER            =
 =============================-->
 
-<header class="container-flui">
+<header class="container-fluid">
 	<div class="container">
+			<div class="row" id="header">
 		
 		<!--==============================
 		=            LOGOTIPO            =
 		===============================-->
-		<div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="header">
+		<div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logotipo">
 
 			<a href="#">
 				
@@ -98,12 +101,174 @@
 		<!--==================================================
 		=            BLOQUE CATEGORIAS Y BUSCADOR            =
 		===================================================-->
+
+		<div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+			
+			<!--================================
+			=            BOTON CATEGORIAS            =
+			=================================-->
+			
+			
+			<div class="col-lg-4 col-md-4  col-sm-4 col-xs-12 backColor" id="btnCategorias">
+				<p>
+					CATEGORIAS
+					<span class="pull-right">
+						<i class="fa fa-bars" aria-hidden="true"></i>
+					</span>
+				</p>
+			</div>
+			
+			<!--====  End of CATEGORIAS  ====-->
+			
 		
-		
-		
+			
+			
+			<!--===============================
+			=            BUSCADOR            =
+			================================-->
+			<div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id="buscador">
+				<input type="search" name="buscar" class="form-control" placeholder="Buscar...">
+				<span class="input-group-btn">
+					<a href="#">
+						<button class="btn btn-default backColor" type="submit">
+							<i class="fa fa-search"></i>
+						</button>
+					</a>
+					
+				</span>
+			</div>				
+			<!--====  End of BUSCADOR  ====-->
+			
+		</div>
 		<!--====  End of BLOQUE CATEGORIAS Y BUSCADOR  ====-->
 		
-		
+		<!--=============================
+			=            CARRITO DE COMPRAS           =
+			==============================-->
+				
+			<div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="carrito">
+				<a href="#">
+					<button class="btn btn-default pull-left backColor">
+						<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+					</button>
+				</a>
+				<p>TU PEDIDO <span class="cantidadCesta"></span> <br> MXN $ <span class="sumaCesta"></span>
+				</p>
+			</div>
+			
+			
+			<!--====  End of CARRITO DE COMPRAS ====-->
+
+			<!--================================
+			=            Categorias            =
+			=================================-->
+			
+			<div class="col-xs-12 backColor" id="categorias">
+				<div class="col-lg-2 col-mx-3 col-sm-4 col-xs-12">
+					<h4>
+						<a href="#" class="pixelCategorias">Lorem Ipsum</a>
+					</h4>
+
+					<hr>
+
+					<ul>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-lg-2 col-mx-3 col-sm-4 col-xs-12">
+					<h4>
+						<a href="#" class="pixelCategorias">Lorem Ipsum</a>
+					</h4>
+
+					<hr>
+
+					<ul>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-lg-2 col-mx-3 col-sm-4 col-xs-12">
+					<h4>
+						<a href="#" class="pixelCategorias">Lorem Ipsum</a>
+					</h4>
+
+					<hr>
+
+					<ul>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-lg-2 col-mx-3 col-sm-4 col-xs-12">
+					<h4>
+						<a href="#" class="pixelCategorias">Lorem Ipsum</a>
+					</h4>
+
+					<hr>
+
+					<ul>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+						<li>
+							<a href="#" class="pixelSubCategorias">Lorem Ipsum</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			
+			<!--====  End of Categorias  ====-->
+			
+
 	</div>
 	
 </header>
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -17,6 +17,11 @@
 
 	<link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
 	<link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu|Ubuntu+Condensed" rel="stylesheet">
+	<link rel="stylesheet" href="vistas/css/plantilla.css">
+	<link rel="stylesheet" href="vistas/css/header.css">
+
 
 	<script src="vistas/js/plugins/jquery.min.js"></script>
 	<script src="vistas/js/plugins/bootstrap.min.js"></script>
