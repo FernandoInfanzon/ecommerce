d31793155345b4396ebfb4e353258071e5b4c9d0
Fernando Infanzon <luisfernandoim@gmail.com>
2018-08-01 20:11:58 -0500

    configuracion inicial

---
+++ vistas/modulos/header.php
@@ -0,0 +1,112 @@
+<!--=========================
+=            TOP            =
+==========================-->
+<div class="container">
+	<div class="row">
+		
+			<!--=====================================
+			=            Section SOCIAL            =
+			======================================-->
+		<div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">	
+			
+			<ul>
+				<li>
+					<a href="http://facebook.com/" target="_blank">
+						<i class="fa fa-facebook redSocial facebookBlanco" aria-hidden="true"></i>
+					</a>
+				</li>
+
+				<li>
+					<a href="http://youtube.com/" target="_blank">
+						<i class="fa fa-youtube redSocial youtubeBlanco" aria-hidden="true"></i>
+					</a>
+				</li>
+
+				<li>
+					<a href="http://twitter.com/" target="_blank">
+						<i class="fa fa-twitter redSocial twitterBlanco" aria-hidden="true"></i>
+					</a>
+				</li>
+
+				<li>
+					<a href="http://google.com/" target="_blank">
+						<i class="fa fa-google-plus redSocial googleBlanco" aria-hidden="true"></i>
+					</a>
+				</li>
+
+				<li>
+					<a href="http://instagram.com/" target="_blank">
+						<i class="fa fa-instagram redSocial instagramBlanco" aria-hidden="true"></i>
+					</a>
+				</li>
+			</ul>
+
+			
+
+		</div>	
+		<!--====  End of Section SOCIAL  ====-->
+		
+		<!--==============================
+		=            REGISTRO            =
+		===============================-->
+		<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 registro">
+			<ul>
+				<li>
+					<a href="#modalIngreso" data-toggle="modal">Log in</a>
+				</li>
+				<li>|</li>
+				<li>
+					<a href="#modalRegistro" data-toggle="modal">Sign In</a>
+				</li>
+
+			</ul>
+		</div>
+		<!--====  End of REGISTRO  ====-->
+			
+
+		
+	</div>
+</div>
+
+
+<!--====  End of TOP  ====-->
+
+<!--============================
+=            HEADER            =
+=============================-->
+
+<header class="container-flui">
+	<div class="container">
+		
+		<!--==============================
+		=            LOGOTIPO            =
+		===============================-->
+		<div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="header">
+
+			<a href="#">
+				
+				<img src="vistas/img/plantilla/logo.png" class="img-responsive" alt="Logo Adelphos">
+
+			</a>
+			
+
+		</div>	
+	
+		
+		<!--====  End of LOGOTIPO  ====-->
+
+		<!--==================================================
+		=            BLOQUE CATEGORIAS Y BUSCADOR            =
+		===================================================-->
+		
+		
+		
+		<!--====  End of BLOQUE CATEGORIAS Y BUSCADOR  ====-->
+		
+		
+	</div>
+	
+</header>
+
+
+<!--====  End of HEADER  ====-->
---
+++ vistas/plantilla.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<meta name="title" content="Tienda Virtual Adelphos">
+	<meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. ">
+
+	<meta name="keyword" content="Duis aute, irure dolor, in reprehenderit, in voluptate, velit esse,
+	cillum dolore eu fugiat, nulla pariatur,. Excepteur sint occaecat, cupidatat non
+	proident, sunt in culpa qui, officia deserunt mollit, anim id est laborum.">
+
+	<title>Tienda Virtual Adelphos Moné</title>
+
+	<link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+	<link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+
+	<script src="vistas/js/plugins/jquery.min.js"></script>
+	<script src="vistas/js/plugins/bootstrap.min.js"></script>
+
+
+
+
+</head>
+<body>
+	
+<?php
+
+/*==============================
+=            Header            =
+==============================*/
+include "modulos/header.php"
+
+
+/*=====  End of Header  ======*/
+
+
+?>
+</body>
+</html>
